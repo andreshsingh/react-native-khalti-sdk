1053091fc66665abaa52039cc3fd6b05eebd7469
aanjan123 <aanjanbhuwan@gmail.com>
2021-06-08 13:57:38 +0545

    fix: server response handled

--- android/src/main/java/com/reactnativekhaltisdk/KhaltiSdkModule.java
+++ android/src/main/java/com/reactnativekhaltisdk/KhaltiSdkModule.java
@@ -11,6 +11,10 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.WritableMap;
+
 import com.khalti.checkout.helper.Config;
 import com.khalti.checkout.helper.KhaltiCheckOut;
 import com.khalti.checkout.helper.PaymentPreference;
@@ -26,7 +30,6 @@ public class KhaltiSdkModule extends ReactContextBaseJavaModule {
    private Config.Builder builder;
    private final ReactApplicationContext reactContext;
    private List<PaymentPreference> defaultPaymentPreferences;
-   private Map<String, Object> tabSelectedMap = new HashMap<>();
 
   KhaltiSdkModule(ReactApplicationContext reactContext) {
        super(reactContext);
@@ -42,35 +45,35 @@ public class KhaltiSdkModule extends ReactContextBaseJavaModule {
     // See https://reactnative.dev/docs/native-modules-android
     @ReactMethod
     public void startKhaltiSdk(String public_key, String product_name, String product_id, String product_url, Double amount, Boolean eBanking, ReadableMap additional_data  , Promise promise) {
-      Log.e("public_key", public_key);
-      Log.e("product_name", product_name);
-      Log.e("product_id", product_id);
-      Log.e("product_url", product_url);
-      Log.e("amount", String.valueOf(amount));
-      Log.e("eBanking", String.valueOf(eBanking));
-      Log.e("additional_data", String.valueOf(additional_data));
+       Log.e("public_key", public_key);
+       Log.e("product_name", product_name);
+       Log.e("product_id", product_id);
+       Log.e("product_url", product_url);
+       Log.e("amount", String.valueOf(amount));
+       Log.e("eBanking", String.valueOf(eBanking));
+       Log.e("additional_data", String.valueOf(additional_data));
 
-       Config eBankingConfig = new Config.Builder(public_key, product_id, product_name, amount.longValue(), new OnCheckOutListener() {
+      Config eBankingConfig = new Config.Builder(public_key, product_id, product_name, amount.longValue(), new OnCheckOutListener() {
          @Override
          public void onError(@NonNull String action, @NonNull Map<String, String> errorMap) {
-           Log.e("error", errorMap.toString());
+           promise.reject("PAYMENT_ERROR", "Payment was unsuccessful");
          }
 
          @Override
          public void onSuccess(@NonNull Map<String, Object> data) {
-           Log.e("success", data.toString());
+           WritableMap map = Arguments.createMap();
+           map.putString("result", String.valueOf(data));
+           promise.resolve(map);
          }
        })
          .paymentPreferences(new ArrayList<PaymentPreference>() {{
            add(PaymentPreference.KHALTI);
-           add(PaymentPreference.EBANKING);
-           add(PaymentPreference.MOBILE_BANKING);
-           add(PaymentPreference.CONNECT_IPS);
-           add(PaymentPreference.SCT);
+            if(eBanking) {
+              add(PaymentPreference.EBANKING);
+            }
          }})
-           .additionalData(tabSelectedMap)
+           .additionalData(additional_data.toHashMap())
            .productUrl(product_url)
-           .mobile("")
            .build();
       Activity currentActivity = getCurrentActivity();
 
