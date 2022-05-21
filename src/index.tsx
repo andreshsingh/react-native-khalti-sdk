33488f3d3c84a25a3db89cebe01f165f5ba81f49
Aashan Ghimire <ashanghimire10@gmail.com>
2022-05-22 02:33:29 +0545

    Added propper response type declaration for typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,14 @@
 import { NativeModules } from 'react-native';
 
+type KhaltiResponse = {
+  mobile: string;
+  product_name: string;
+  product_identity: string;
+  product_url?: string;
+  amount: number;
+  token: string;
+  idx: string;
+}
 type KhaltiSdkType = {
   startKhaltiSdk(
     merchantKey: string,
@@ -9,7 +18,7 @@ type KhaltiSdkType = {
     amount: number,
     additionalData: object,
     KhaltiAppScheme: string
-  ): void;
+  ): KhaltiResponse;
 };
 
 const { KhaltiSdk } = NativeModules;
